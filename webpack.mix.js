d9c007cdc26e9826fcd797092fb103bdceb78ca5
Samuel Girardin <samuelgirardin@pm.me>
2020-09-18 18:31:34 +0200

    try to fix vue compilation

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,8 @@
 const cssImport = require('postcss-import')
 const cssNesting = require('postcss-nesting')
 const mix = require('laravel-mix')
-const path = require('path')
-const purgecss = require('@fullhuman/postcss-purgecss')
+// const path = require('path')
+// const purgecss = require('@fullhuman/postcss-purgecss')
 const tailwindcss = require('tailwindcss')
 
 /*
@@ -17,29 +17,30 @@ const tailwindcss = require('tailwindcss')
  */
 
 mix.js('resources/js/app.js', 'storage/public/js')
-  .postCss('resources/css/app.css', 'storage/public/css')
+  .postCss('resources/css/app.css', 'storage/public/css/app.css')
   .options({
     postCss: [
       cssImport(),
       cssNesting(),
       tailwindcss('tailwind.config.js'),
-      ...mix.inProduction() ? [
-        purgecss({
-          content: ['./resources/templates/**/*.html', './resources/js/**/*.vue'],
-          defaultExtractor: content => content.match(/[\w-/:.]+(?<!:)/g) || [],
-          whitelistPatternsChildren: [/nprogress/],
-        }),
-      ] : [],
+      [],
+  //     ...mix.inProduction() ? [
+  //       purgecss({
+  //         content: ['./resources/templates/**/*.html', './resources/js/**/*.vue'],
+  //         defaultExtractor: content => content.match(/[\w-/:.]+(?<!:)/g) || [],
+  //         whitelistPatternsChildren: [/nprogress/],
+  //       }),
+  //     ] : [],
     ],
   })
-  .webpackConfig({
-    output: { chunkFilename: 'js/[name].js?id=[chunkhash]' },
-    resolve: {
-      alias: {
-        vue$: 'vue/dist/vue.runtime.esm.js',
-        '@': path.resolve('resources/js'),
-      },
-    },
-  })
-  .version()
-  .sourceMaps()
+  // .webpackConfig({
+  //   output: { chunkFilename: 'storage/public/js/[name].js?id=[chunkhash]' },
+  //   resolve: {
+  //     alias: {
+  //       vue$: 'vue/dist/vue.runtime.esm.js',
+  //       '@': path.resolve('resources/js'),
+  //     },
+  //   },
+  // })
+  // .version()
+  // .sourceMaps()
