e44214a51cf0b7a0eddd7eed5af3447196ba9321
Samuel Girardin <samuelgirardin@pm.me>
2021-01-09 13:35:21 +0100

    switch to env vars in database config

--- config/database.py
+++ config/database.py
@@ -21,53 +21,53 @@ They can be named whatever you want.
 """
 
 DATABASES = {
-    'default': 'postgres',
-    'mysql': {
-        'driver': 'mysql',
-        'host': env('MYSQL_DATABASE_HOST'),
-        'user': env('MYSQL_DATABASE_USER'),
-        'password': env('MYSQL_DATABASE_PASSWORD'),
-        'database': env('MYSQL_DATABASE_DATABASE'),
-        'port': env('MYSQL_DATABASE_PORT'),
-        'prefix': '',
+    "default": "postgres",
+    "mysql": {
+        "driver": "mysql",
+        "host": env("MYSQL_DATABASE_HOST"),
+        "user": env("MYSQL_DATABASE_USER"),
+        "password": env("MYSQL_DATABASE_PASSWORD"),
+        "database": env("MYSQL_DATABASE_DATABASE"),
+        "port": env("MYSQL_DATABASE_PORT"),
+        "prefix": "",
         "grammar": "mysql",
-        'options': {
-            'charset': 'utf8mb4',
+        "options": {
+            "charset": "utf8mb4",
         },
-        'log_queries': True
+        "log_queries": True,
     },
-    'postgres': {
-        'driver': 'postgres',
-        'host': 'localhost',
-        'user': 'samuel',
-        'password': 'samuel',
-        'database': 'pingcrm_masonite',
-        'port': 5432,
-        'prefix': '',
-        'log_queries': True
+    "postgres": {
+        "driver": "postgres",
+        "host": env("DB_HOST"),
+        "user": env("DB_USERNAME"),
+        "password": env("DB_PASSWORD"),
+        "database": env("DB_DATABASE"),
+        "port": env("DB_PORT"),
+        "prefix": "",
+        "log_queries": True,
     },
-    'sqlite': {
-        'driver': 'sqlite',
-        'database': 'orm.sqlite3',
-        'prefix': '',
-        'log_queries': True
+    "sqlite": {
+        "driver": "sqlite",
+        "database": "orm.sqlite3",
+        "prefix": "",
+        "log_queries": True,
     },
-    'mssql': {
-        'driver': 'mssql',
-        'host': env('MSSQL_DATABASE_HOST'),
-        'user': env('MSSQL_DATABASE_USER'),
-        'password': env('MSSQL_DATABASE_PASSWORD'),
-        'database': env('MSSQL_DATABASE_DATABASE'),
-        'port': env('MSSQL_DATABASE_PORT'),
-        'prefix': '',
-        'log_queries': True
+    "mssql": {
+        "driver": "mssql",
+        "host": env("MSSQL_DATABASE_HOST"),
+        "user": env("MSSQL_DATABASE_USER"),
+        "password": env("MSSQL_DATABASE_PASSWORD"),
+        "database": env("MSSQL_DATABASE_DATABASE"),
+        "port": env("MSSQL_DATABASE_PORT"),
+        "prefix": "",
+        "log_queries": True,
     },
 }
 
 DB = ConnectionResolver().set_connection_details(DATABASES)
 
 
-logger = logging.getLogger('masoniteorm.connection.queries')
+logger = logging.getLogger("masoniteorm.connection.queries")
 logger.setLevel(logging.DEBUG)
 
 handler = logging.StreamHandler()
