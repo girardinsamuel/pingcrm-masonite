14efec931e9de89460f543d8c5ec91d6caef87ef
Samuel Girardin <samuelgirardin@pm.me>
2021-05-06 18:14:56 +0200

    update

--- config/factories.py
+++ config/factories.py
@@ -4,38 +4,54 @@ from masoniteorm import Factory
 from masonite.helpers import password
 from app.User import User
 from app.Organization import Organization
+from app.Contact import Contact
 
 
 def get_random_string(length):
     letters = string.ascii_lowercase
-    result_str = ''.join(random.choice(letters) for i in range(length))
+    result_str = "".join(random.choice(letters) for i in range(length))
     return result_str
 
 
-def user_factory(faker):
+def user_factory(self, faker):
     return {
-        'first_name': faker.first_name(),
-        'last_name': faker.last_name(),
-        'email': faker.email(),
-        'password': password('secret'),
-        'remember_token': get_random_string(10),
-        'owner': False,
-        'photo_path': ''
+        "first_name": faker.first_name(),
+        "last_name": faker.last_name(),
+        "email": faker.email(),
+        "password": password("secret"),
+        "remember_token": get_random_string(10),
+        "owner": False,
+        "photo_path": "",
     }
 
 
-def organization_factory(faker):
+def organization_factory(self, faker):
     return {
-        'name': faker.company(),
-        'email': faker.email(),
-        'phone': "0656453423",
-        'address': faker.address(),
-        'city': faker.city(),
-        'region': faker.state(),
-        'country': 'US',
-        'postal_code': faker.postcode()
+        "name": faker.name(),
+        "email": faker.email(),
+        "phone": faker.phone(),
+        "address": faker.address(),
+        "city": faker.city(),
+        "region": faker.region(),
+        "country": "US",
+        "postal_code": faker.post_code(),
+    }
+
+
+def contact_factory(self, faker):
+    return {
+        "first_name": faker.first_name(),
+        "last_name": faker.last_name(),
+        "email": faker.email(),
+        "phone": faker.phone(),
+        "address": faker.address(),
+        "city": faker.city(),
+        "region": faker.region(),
+        "country": "US",
+        "postal_code": faker.post_code(),
     }
 
 
 Factory.register(User, user_factory)
 Factory.register(Organization, organization_factory)
+Factory.register(Contact, contact_factory)
--- databases/seeds/database_seeder.py
+++ databases/seeds/database_seeder.py
@@ -1,11 +1,21 @@
 """Base Database Seeder Module."""
 from masoniteorm.seeds import Seeder
+from config.factories import Factory
+import random
 
-from .user_table_seeder import UserTableSeeder
+from app.Account import Account
+from app.Organization import Organization
+from app.Contact import Contact
 
 
 class DatabaseSeeder(Seeder):
-
     def run(self):
         """Run the database seeds."""
-        self.call(UserTableSeeder)
+        # self.call(UserTableSeeder)
+        account = Account.first()
+
+        # organizations = Factory(Organization, 100).create({"account_id": account.id})
+        organizations = Organization.all()
+        contacts = Factory(Contact, 100).create({"account_id": account.id})
+        for c in contacts:
+            c.update({"organization_id": organizations[random.randint(0, 99)].id})
